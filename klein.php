ff3840ebdbc1827e7821e32e353c39599742bc6c
Trevor Suarez <rican7@gmail.com>
2012-11-29 03:14:45 -0500

    Whoops. I knew there was a reason I was using array_merge before. ( $method can be a string OR an array ... this was firing an annoying PHP Notice after further testing )

--- klein.php
+++ klein.php
@@ -205,7 +205,7 @@ function dispatch($uri = null, $req_method = null, array $params = null, $captur
 
         if (isset($match) && $match ^ $negate) {
              // Keep track of possibly matched methods
-             $methods_matched[] = $method;
+             $methods_matched = array_merge( $methods_matched, (array) $method );
              $methods_matched = array_filter( $methods_matched );
              $methods_matched = array_unique( $methods_matched );
 
