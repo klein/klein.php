8c9703c4fddf6ed9dbe91960c03d26bbd75419c4
Gilles Bouthenot <gilles@mate-vdi>
2013-03-01 14:29:29 +0100

    Add test to highlight that .[:format] parsing cannot be used

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -110,4 +110,23 @@ class RoutesTest extends PHPUnit_Framework_TestCase {
 		respond( '404', function(){ echo '404 Code'; } );
 		dispatch( '/blue' );
 	}
+
+	public function testDot1() {
+		$this->expectOutputString( 'matchA:slug=ABCD_E--matchB:slug=ABCD_E--' );
+
+		respond('/[*:cpath]/[:slug].[:format]',   function($rq){ echo 'matchA:slug='.$rq->param("slug").'--';});
+		respond('/[*:cpath]/[:slug].[:format]?',  function($rq){ echo 'matchB:slug='.$rq->param("slug").'--';});
+		respond('/[*:cpath]/[a:slug].[:format]?', function($rq){ echo 'matchC:slug='.$rq->param("slug").'--';});
+		dispatch("/category1/categoryX/ABCD_E.php");
+	}
+
+	public function testDot2() {
+		$this->expectOutputString( 'matchB:slug=ABCD_E--' );
+
+		respond('/[*:cpath]/[:slug].[:format]',   function($rq){ echo 'matchA:slug='.$rq->param("slug").'--';});
+		respond('/[*:cpath]/[:slug].[:format]?',  function($rq){ echo 'matchB:slug='.$rq->param("slug").'--';});
+		respond('/[*:cpath]/[a:slug].[:format]?', function($rq){ echo 'matchC:slug='.$rq->param("slug").'--';});
+		dispatch("/category1/categoryX/ABCD_E");
+	}
+
 }
