b4e9613ec7f7cf54b8c3fe1455f8c712fa8158d8
Trevor Suarez <rican7@gmail.com>
2013-03-22 02:23:57 -0400

    Adding new test _Headers classes

--- tests/setup.php
+++ tests/setup.php
@@ -8,4 +8,24 @@ class HeadersEcho extends _Headers {
 	}
 }
 
+class HeadersNoOp extends _Headers {
+	public function header($key, $value = null) {
+		// Do nothing. ;)
+	}
+}
+
+class HeadersSave extends _Headers {
+	public $headers_values = array();
+
+	public function __construct( &$headers_array_ref ) {
+		$this->headers_values = &$headers_array_ref;
+	}
+
+	public function header($key, $value = null) {
+		$this->headers_values[] = $this->_header($key, $value) . "\n";
+
+		return $this->headers_values;
+	}
+}
+
 _Request::$_headers = _Response::$_headers = new HeadersEcho;
