21868db16e38829871c02f8f67817d93cc261e79
Trevor Suarez <rican7@gmail.com>
2013-03-27 10:50:33 -0400

    Similarly to the other tests, the ValidationsTest class is now using the "auto-instance" of Klein for quick and easy testing. :)

--- tests/ValidationsTest.php
+++ tests/ValidationsTest.php
@@ -8,8 +8,8 @@ class ValidationsTest extends AbstractKleinTest {
 		parent::setUp();
 
         // Setup our error handler
-        respond( function( $request, $response ) {
-            $response->onError( array( $this, 'errorHandler' ) );
+        $this->klein_app->respond( function( $request, $response ) {
+            $response->onError( array( $this, 'errorHandler' ), false );
         } );
 	}
 
@@ -25,7 +25,7 @@ class ValidationsTest extends AbstractKleinTest {
     public function testCustomValidationMessage() {
         $custom_message = 'This is a custom error message...';
 
-		respond( '/[:test_param]', function( $request ) use ( $custom_message ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) use ( $custom_message ) {
 			$request->validate( 'test_param', $custom_message )
 			        ->notNull()
 			        ->isLen( 0 );
@@ -34,11 +34,11 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( $custom_message, function(){ dispatch('/test'); });
+		$this->assertOutputSame( $custom_message, function(){ $this->klein_app->dispatch('/test'); });
     }
 
 	public function testStringLengthExact() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isLen( 2 );
@@ -47,12 +47,12 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/ab'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/ab'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test'); });
 	}
 
 	public function testStringLengthRange() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isLen( 3, 5 );
@@ -61,17 +61,17 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/dog'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/dogg'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/doggg'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/t'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/te'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/testin'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/testing'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/dog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/dogg'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/doggg'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/t'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/te'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/testin'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/testing'); });
 	}
 
 	public function testInt() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isInt();
@@ -80,17 +80,17 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/2'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/12318935'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2.5'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2,5'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/~2'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2 5'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/2'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/12318935'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2.5'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2,5'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/~2'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2 5'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test'); });
 	}
 
 	public function testFloat() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isFloat();
@@ -99,18 +99,18 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/2'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/2.5'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/3.14'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2.'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2,5'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/~2'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2 5'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/2'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/2.5'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/3.14'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2.'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2,5'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/~2'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2 5'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test'); });
 	}
 
 	public function testEmail() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isEmail();
@@ -119,15 +119,15 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/test@test.com'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/test@test.co.uk'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test@'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/2 5'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/test@test.com'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/test@test.co.uk'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test@'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/2 5'); });
 	}
 
 	public function testAlpha() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isAlpha();
@@ -136,17 +136,17 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/test'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/Test'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/TesT'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/test1'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/1test'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/@test'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/-test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/Test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/TesT'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/test1'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/1test'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/@test'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/-test'); });
 	}
 
 	public function testAlnum() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isAlnum();
@@ -155,17 +155,17 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/test'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/Test'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/TesT'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/test1'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/1test'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/@test'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/-test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/Test'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/TesT'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/test1'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/1test'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/@test'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/-test'); });
 	}
 
 	public function testContains() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->contains( 'dog' );
@@ -174,16 +174,16 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/bigdog'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/dogbig'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat-dog'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/catdogbear'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/DOG'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/doog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/bigdog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/dogbig'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat-dog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/catdogbear'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/DOG'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/doog'); });
 	}
 
 	public function testChars() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isChars( 'c-f' );
@@ -192,15 +192,15 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cdef'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cfed'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cf'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cdefg'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/dog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cdef'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cfed'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cf'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cdefg'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/dog'); });
 	}
 
 	public function testRegex() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isRegex( '/cat-[dog|bear|thing]/' );
@@ -209,17 +209,17 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat-dog'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat-bear'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat-thing'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cat'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cat-'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/dog-cat'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/catdog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat-dog'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat-bear'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat-thing'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cat'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cat-'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/dog-cat'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/catdog'); });
 	}
 
 	public function testNotRegex() {
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->notRegex( '/cat-[dog|bear|thing]/' );
@@ -228,24 +228,24 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/cat-'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/dog-cat'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/catdog'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cat-dog'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cat-bear'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/cat-thing'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/cat-'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/dog-cat'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/catdog'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cat-dog'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cat-bear'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/cat-thing'); });
 	}
 
 	public function testCustomValidator() {
         // Add our custom validator
-        addValidator( 'donkey', function( $string, $color ) {
+        $this->klein_app->addValidator( 'donkey', function( $string, $color ) {
             $regex_str = $color . '[-_]?donkey';
 
             return preg_match( '/' . $regex_str . '/', $string );
         });
 
-		respond( '/[:test_param]', function( $request ) {
+		$this->klein_app->respond( '/[:test_param]', function( $request ) {
 			$request->validate( 'test_param' )
 			        ->notNull()
 			        ->isDonkey( 'brown' );
@@ -254,13 +254,13 @@ class ValidationsTest extends AbstractKleinTest {
             echo 'yup!';
 		} );
 
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/browndonkey'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/brown-donkey'); });
-		$this->assertOutputSame( 'yup!', function(){ dispatch('/brown_donkey'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/bluedonkey'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/blue-donkey'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/blue_donkey'); });
-		$this->assertOutputSame( 'fail', function(){ dispatch('/brown_donk'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/browndonkey'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/brown-donkey'); });
+		$this->assertOutputSame( 'yup!', function(){ $this->klein_app->dispatch('/brown_donkey'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/bluedonkey'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/blue-donkey'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/blue_donkey'); });
+		$this->assertOutputSame( 'fail', function(){ $this->klein_app->dispatch('/brown_donk'); });
 	}
 
 }
