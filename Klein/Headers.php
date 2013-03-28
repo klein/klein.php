a7f816f38135f23d371ce551129279641614f82f
Trevor Suarez <rican7@gmail.com>
2013-03-28 01:10:41 -0400

    New function name, and new PHPDocs

--- Klein/Headers.php
+++ Klein/Headers.php
@@ -19,22 +19,38 @@ namespace Klein;
  */
 class Headers {
 
-    public function header($key, $value = null) {
-        header($this->header($key, $value));
+    /**
+	 * Set an HTTP header by first creating
+	 * a header string from our passed values
+     * 
+     * @param string $key
+     * @param string $value
+     * @access public
+     * @return void
+     */
+    public function header( $key, $value = null ) {
+		header(
+			$this->create_header_string( $key, $value )
+		);
     }
 
     /**
      * Output an HTTP header. If $value is null, $key is
      * assume to be the HTTP response code, and the ":"
      * separator will be omitted.
+     * 
+     * @param string $key
+     * @param string $value
+     * @access public
+     * @return string
      */
-    public function _header($key, $value = null) {
-        if (null === $value) {
+    public function create_header_string( $key, $value = null ) {
+        if ( null === $value ) {
             return $key;
         }
 
-        $key = str_replace(' ', '-', ucwords(str_replace('-', ' ', $key)));
-        return "$key: $value";
+        $key = str_replace( ' ', '-', ucwords( str_replace( '-', ' ', $key ) ) );
+        return $key . ': ' . $value;
     }
 
 } // End class Headers
