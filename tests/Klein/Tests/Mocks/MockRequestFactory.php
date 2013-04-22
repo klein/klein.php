a66673e0f0768e8843261f1c6645a6f996ee2ba2
Trevor Suarez <rican7@gmail.com>
2013-04-22 01:24:35 -0400

    New test class for mocking requests easily

---
+++ tests/Klein/Tests/Mocks/MockRequestFactory.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Klein (klein.php) - A lightning fast router for PHP
+ *
+ * @author      Chris O'Hara <cohara87@gmail.com>
+ * @author      Trevor Suarez (Rican7) (contributor and v2 refactorer)
+ * @copyright   (c) Chris O'Hara
+ * @link        https://github.com/chriso/klein.php
+ * @license     MIT
+ */
+
+namespace Klein\Tests\Mocks;
+
+use \Klein\Request;
+
+/**
+ * MockRequestFactory 
+ *
+ * Allow for the simple creation of mock requests
+ * (great for testing... ;))
+ *
+ * @package Klein\Tests\Mocks
+ */
+class MockRequestFactory
+{
+
+    /**
+     * Create a new mock request
+     *
+     * @param string $uri
+     * @param string $req_method
+     * @param array $parameters
+     * @param array $cookies
+     * @param array $server
+     * @param array $files
+     * @param string $body
+     * @static
+     * @access public
+     * @return void
+     */
+    public static function create(
+        $uri = '/',
+        $req_method = 'GET',
+        $parameters = array(),
+        $cookies = array(),
+        $server = array(),
+        $files = array(),
+        $body = null
+    ) {
+        // Create a new Request object
+        $request = new Request(
+            array(),
+            array(),
+            $cookies,
+            $server,
+            $files,
+            $body
+        );
+
+        // Reformat
+        $req_method = strtoupper(trim($req_method));
+
+        // Set its URI and Method
+        $request->server()->set('REQUEST_URI', $uri);
+        $request->server()->set('REQUEST_METHOD', $req_method);
+
+        // Set our parameters
+        switch ($req_method) {
+            case 'POST':
+            case 'PUT':
+            case 'PATCH':
+            case 'DELETE':
+                $request->paramsPost()->replace($parameters);
+                break;
+            default:
+                $request->paramsGet()->replace($parameters);
+                break;
+        }
+
+        return $request;
+    }
+}
