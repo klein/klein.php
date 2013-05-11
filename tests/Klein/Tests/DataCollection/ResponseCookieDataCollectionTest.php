53548d388e45b07c916b66399dc19a636d79ae78
Trevor Suarez <rican7@gmail.com>
2013-05-11 03:08:42 -0400

    Tests for the ResponseCookieDataCollection class

---
+++ tests/Klein/Tests/DataCollection/ResponseCookieDataCollectionTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Klein (klein.php) - A lightning fast router for PHP
+ *
+ * @author      Chris O'Hara <cohara87@gmail.com>
+ * @author      Trevor Suarez (Rican7) (contributor and v2 refactorer)
+ * @copyright   (c) Chris O'Hara
+ * @link        https://github.com/chriso/klein.php
+ * @license     MIT
+ */
+
+namespace Klein\Tests\DataCollection;
+
+use \Klein\Tests\AbstractKleinTest;
+use \Klein\DataCollection\ResponseCookieDataCollection;
+use \Klein\ResponseCookie;
+
+/**
+ * ResponseCookieDataCollectionTest 
+ *
+ * @uses AbstractKleinTest
+ * @package Klein\Tests\DataCollection
+ */
+class ResponseCookieDataCollectionTest extends AbstractKleinTest
+{
+
+    /*
+     * Data Providers and Methods
+     */
+
+    /**
+     * Sample data provider
+     *
+     * @access public
+     * @return array
+     */
+    public function sampleDataProvider()
+    {
+        $sample_cookie = new ResponseCookie(
+            'Trevor',
+            'is a programmer',
+            3600,
+            '/',
+            'example.com',
+            false,
+            false
+        );
+
+        $sample_other_cookie = new ResponseCookie(
+            'Chris',
+            'is a boss',
+            60,
+            '/app/',
+            'github.com',
+            true,
+            true
+        );
+
+        return array(
+            array($sample_cookie, $sample_other_cookie),
+        );
+    }
+
+
+    /*
+     * Tests
+     */
+
+    /**
+     * @dataProvider sampleDataProvider
+     */
+    public function testSet($sample_cookie, $sample_other_cookie)
+    {
+        // Create our collection with NO data
+        $data_collection = new ResponseCookieDataCollection();
+
+        // Set our data from our test data
+        $data_collection->set('first', $sample_cookie);
+
+        $this->assertSame($sample_cookie, $data_collection->get('first'));
+        $this->assertTrue($data_collection->get('first') instanceof ResponseCookie);
+    }
+
+    public function testSetStringConvertsToCookie()
+    {
+        // Create our collection with NO data
+        $data_collection = new ResponseCookieDataCollection();
+
+        // Set our data from our test data
+        $data_collection->set('first', 'value');
+
+        $this->assertNotSame('value', $data_collection->get('first'));
+        $this->assertTrue($data_collection->get('first') instanceof ResponseCookie);
+    }
+
+    /**
+     * @dataProvider sampleDataProvider
+     */
+    public function testConstructorRoutesThroughSet($sample_cookie, $sample_other_cookie)
+    {
+        $array_of_cookie_instances = array(
+            $sample_cookie,
+            $sample_other_cookie,
+            new ResponseCookie('test'),
+        );
+
+        // Create our collection with NO data
+        $data_collection = new ResponseCookieDataCollection($array_of_cookie_instances);
+        $this->assertSame($array_of_cookie_instances, $data_collection->all());
+
+        foreach ($data_collection as $cookie) {
+            $this->assertTrue($cookie instanceof ResponseCookie);
+        }
+    }
+}
