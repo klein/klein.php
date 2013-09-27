27e65081fa1da980bb309a4bcdc8bd1681573dbc
Trevor Suarez <rican7@gmail.com>
2013-09-27 14:44:39 -0400

    Making session methods testable by disabling their header output

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,10 @@
  * @license     MIT
  */
 
+// Set some configuration values
+ini_set('session.use_cookies', 0);      // Don't send headers when testing sessions
+ini_set('session.cache_limiter', '');   // Don't send cache headers when testing sessions
+
 // Load our autoloader, and add our Test class namespace
 $autoloader = require(__DIR__ . '/../vendor/autoload.php');
 $autoloader->add('Klein\Tests', __DIR__);
