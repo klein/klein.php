a7906d3273ad5d16e1336adcec75bd7ac4424372
Trevor Suarez <rican7@gmail.com>
2014-01-04 04:08:43 -0500

    Finally making the awkward "functions-bootstrap.php" file PSR-2 compliant

--- tests/functions-bootstrap.php
+++ tests/functions-bootstrap.php
@@ -1,26 +1,41 @@
 <?php
-// @codingStandardsIgnoreFile
+/**
+ * Klein (klein.php) - A lightning fast router for PHP
+ *
+ * @author      Chris O'Hara <cohara87@gmail.com>
+ * @author      Trevor Suarez (Rican7) (contributor and v2 refactorer)
+ * @copyright   (c) Chris O'Hara
+ * @link        https://github.com/chriso/klein.php
+ * @license     MIT
+ */
 
-// Really exploiting some functional/global PHP behaviors here. :P
-function implement_custom_fastcgi_function() {
+/**
+ * Really exploiting some functional/global PHP behaviors here. :P
+ */
+function implement_custom_fastcgi_function()
+{
     // Check if the function doesn't exist
     if (!function_exists('fastcgi_finish_request')) {
         // Let's just define it then
-        function fastcgi_finish_request() {
+        function fastcgi_finish_request()
+        {
             echo 'fastcgi_finish_request';
         }
     }
 }
 
-function implement_custom_apc_cache_functions() {
+function implement_custom_apc_cache_functions()
+{
     // Check if the function doesn't exist
     if (!function_exists('apc_fetch')) {
 
-        function apc_fetch($key) {
+        function apc_fetch($key)
+        {
             return false;
         }
 
-        function apc_store($key, $value) {
+        function apc_store($key, $value)
+        {
             return false;
         }
     }
