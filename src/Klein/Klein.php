330cfd65c3ccdf1b68245425db1b05beec6ad985
Jason Schilling <jason.schilling@hotmail.de>
2014-03-26 15:07:16 +0100

    Make the HTTP Error codes Executeable
    Make the HTTP Error codes Executeable from external with
    $router->triggerHttpError(404);

--- src/Klein/Klein.php
+++ src/Klein/Klein.php
@@ -892,6 +892,25 @@ class Klein
     }
 
     /**
+     * Trigger and HTTP error from external
+     *
+     * @param integer $code                             The HTTP Status code
+     * @access public
+     * @return void
+     */
+     public function triggerHttpError($code){
+         try {
+			throw HttpException::createFromCode($code);
+		} catch (HttpExceptionInterface $e) {
+        	$locked = $this->response()->isLocked();
+        	$this->httpError($e, new RouteCollection(), array());
+        	if (!$locked) {
+            	$this->response()->unlock();
+        	}
+        }
+     }
+
+    /**
      * Handles an HTTP error exception through our HTTP error callbacks
      *
      * @param HttpExceptionInterface $http_exception    The exception that occurred
